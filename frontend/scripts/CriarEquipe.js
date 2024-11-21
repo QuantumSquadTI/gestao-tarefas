75493d9a165c852a485d856ceb380db7bcde33ee
GustavoNakamuraC <gustavonakamura6@gmail.com>
2024-11-21 20:21:10 -0300

    integração iniciada

--- frontend/scripts/CriarEquipe.js
+++ frontend/scripts/CriarEquipe.js
@@ -1,25 +1,60 @@
 const teamForm = document.getElementById('teamForm');
-const maxTeams = 3; // Número máximo de equipes por usuário
-let createdTeams = 0; // Contador de equipes criadas
+const nome = document.getElementById('teamName');
+const desc = document.getElementById('teamDescription');
+const img = document.getElementById('teamImage');
+
+const body = {
+  nome: nome,
+  descricao: desc,
+  fotoEquipe: img
+}
 
 teamForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  if (createdTeams >= maxTeams) {
-    alert('Você já atingiu o limite máximo de equipes.');
-    return;
-  }
+  axios.post('http://localhost:3000/equipe/1', body)
+    .then(response =>{
+      console.log('Dados enviados com sucesso:', response.data)
+    })
+    .catch(error => {
+      console.error('Erro ao enviar dados:', error)
+    });
 
-  const teamName = document.getElementById('teamName').value.trim();
-  const teamDescription = document.getElementById('teamDescription').value.trim();
-  const teamImage = document.getElementById('teamImage').files[0];
+    //ver como add token
 
-  if (!teamName || !teamDescription) {
-    alert('Todos os campos são obrigatórios.');
-    return;
-  }
 
-  createdTeams++;
-  alert(`Equipe "${teamName}" criada com sucesso!`);
-  teamForm.reset();
-});
+
+
+  // const jsonDados = JSON.stringify(dados)
+
+  // console.log(jsonDados);
+  
+
+  // axios.post('http://localhost:3000/equipe', )
+
+
+  // if (createdTeams >= maxTeams) {
+  //   alert('Você já atingiu o limite máximo de equipes.');
+  //   return;
+  // }
+})
+
+
+// teamForm.addEventListener('submit', (e) => {
+//   e.preventDefault();
+
+  
+
+//   const teamName = document.getElementById('teamName').value.trim();
+//   const teamDescription = document.getElementById('teamDescription').value.trim();
+//   const teamImage = document.getElementById('teamImage').files[0];
+
+//   if (!teamName || !teamDescription) {
+//     alert('Todos os campos são obrigatórios.');
+//     return;
+//   }
+
+//   createdTeams++;
+//   alert(`Equipe "${teamName}" criada com sucesso!`);
+//   teamForm.reset();
+// });
