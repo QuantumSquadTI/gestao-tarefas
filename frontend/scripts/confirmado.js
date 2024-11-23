c6d1c72f05dbd86edb84137cd166614266360532
leticia <leticiasatiko@gmail.com>
2024-11-23 13:19:22 -0300

    commit 2

--- frontend/scripts/confirmado.js
+++ frontend/scripts/confirmado.js
@@ -1,4 +1,3 @@
 document.getElementById("loginButton").addEventListener("click", function () {
-    window.location.href = "login.html"; // Redireciona para a página de login
+    window.location.href = "../scripts/login.html";
   });
-  
